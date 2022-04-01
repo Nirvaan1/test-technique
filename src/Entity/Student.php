8fce1df36a4cb25b2bacb97eb482b89e3d2e3d46
Nirvaan Guilloux <nirvaanguilloux@macbook-air-de-nirvaan-1.home>
2022-04-01 15:21:19 +0200

    create Student Entity and Repository

---
+++ src/Entity/Student.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StudentRepository::class)
+ */
+class Student
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=25)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=25)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $num_etud;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getNumEtud(): ?int
+    {
+        return $this->num_etud;
+    }
+
+    public function setNumEtud(?int $num_etud): self
+    {
+        $this->num_etud = $num_etud;
+
+        return $this;
+    }
+}

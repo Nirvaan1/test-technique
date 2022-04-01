ce2f1be2b11d0aeb2dd8aad3fa758d5af2673d25
Nirvaan Guilloux <nirvaanguilloux@macbook-air-de-nirvaan-1.home>
2022-04-01 16:57:42 +0200

    Add department to the student type

--- src/Form/StudentType.php
+++ src/Form/StudentType.php
@@ -2,9 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Department;
 use App\Entity\Student;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,7 +18,11 @@ class StudentType extends AbstractType
         $builder
             ->add('firstname')
             ->add('lastname')
-            ->add('num_etud')
+            ->add('num_etud', TelType::class)
+            ->add('department', EntityType::class,[
+                'class' => Department::class,
+                'choice_label' => 'name',
+            ])
         ;
     }
 
